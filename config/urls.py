2e51cda954ed5e5786da90cea8bdae85a678e624
GKI Serpong <gkiserpongit@gmail.com>
2022-10-29 14:43:59 +0700

    Put index view

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from anggota.views import import_view
 
 
 urlpatterns = [
+    path('', input_view, name='input-view-index'),
     path('admin/', admin.site.urls),
     path('api/', include('anggota.urls'))
 ]
